2f9013893fe66e48880cfd5610ee317f1d4da9f1
princechaurashiya <princechaurashiya163@gmail.com>
2024-11-10 16:29:02 +0530

    Deploy to Heroku

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const connection = mysql.createConnection(
+  process.env.JAWSDB_URL || {  // Use JAWSDB_URL if available, otherwise use local .env variables
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }
+);
 
 connection.connect((err) => {
   if (err) {
